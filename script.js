d9b22684d9fef92786108837d2209e211fed8f9a
thekaranjit <thekaranjit@outlook.com>
2022-06-24 20:41:24 -0400

    Final

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hi");
+function dis(val) {
+
+  document.getElementById("result").value +=val
+
+  console.log(val)
+}
+
+
+function solve(val) {
+
+  let x = document.getElementById("result").value
+  let y = eval(x)
+  document.getElementById("result").value = y
+
+}
+
+
+function clr(){
+
+document.getElementById("result").value = ""
+
+}
